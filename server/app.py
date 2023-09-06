c25502c9ce1a9e0d220853a822b3082771ed5989
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-09-05 18:44:14 -0700

    adds lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,38 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route("/print/<string:parameter>")
+def print_string(parameter):
+    print (parameter)
+    return parameter
+
+@app.route("/count/<int:parameter>")
+def count(parameter):
+    r = ""
+    for num in range(0, parameter):
+        print(f"{num}")
+        r = r + str(num) + "\n"
+    return r
+
+@app.route("/math/<int:num1>/<string:operation>/<int:num2>")
+def math (num1, operation, num2):
+    answer = 0
+    if operation == 'div':
+        answer = num1/num2
+    elif operation == '+':
+        answer = num1+num2
+    elif operation == '-':
+        answer = num1-num2
+    elif operation == '*':
+        answer = num1*num2
+    else:
+        answer = num1%num2
+    return str(answer)
+        
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
